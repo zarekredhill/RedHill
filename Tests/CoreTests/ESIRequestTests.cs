dcda5f1083d4400ed8cbe4a5a4bf897deced1df0
Zarek RedHill <zarekredhill@gmail.com>
2017-06-02 11:23:52 -0400

    Tidying up test class

--- Tests/CoreTests/ESIRequestTests.cs
+++ Tests/CoreTests/ESIRequestTests.cs
@@ -4,36 +4,33 @@ using Xunit;
 using RedHill.Core;
 
 namespace RedHill.CoreTests
-{
-
-    public class MyFixture : IDisposable
-    {
-        private ILoggerFactory LogFactory { get; }
-        public ESIRequestHandler ESIRequestHandler { get; }
-
-        public MyFixture()
-        {
-            LogFactory = new LoggerFactory();
-            ESIRequestHandler = new ESIRequestHandler("https://esi.tech.ccp.is", "latest", "tranquility", LogFactory.CreateLogger(typeof(ESIRequestHandler)));
-        }
-
-        void IDisposable.Dispose()
-        {
-            LogFactory.Dispose();
-        }
-    }
-    
+{   
     [CollectionDefinition(nameof(ESIRequestTests))]
-    public class MyCollectionFixture : ICollectionFixture<MyFixture>
+    public class MyCollectionFixture : ICollectionFixture<ESIRequestTests.Fixture>
     {
-
     }
 
     [Collection(nameof(ESIRequestTests))]
-    public class ESIRequestTests : IClassFixture<MyFixture>
+    public class ESIRequestTests : IClassFixture<ESIRequestTests.Fixture>
     {
-        private readonly MyFixture _fixture;
-      public ESIRequestTests(MyFixture fixture)
+        public class Fixture: IDisposable
+        {
+            private ILoggerFactory LogFactory { get; }
+            public ESIRequestHandler ESIRequestHandler { get; }
+
+            public Fixture()
+            {
+                LogFactory = new LoggerFactory();
+                ESIRequestHandler = new ESIRequestHandler("https://esi.tech.ccp.is", "latest", "tranquility", LogFactory.CreateLogger(typeof(ESIRequestHandler)));
+            }
+
+            void IDisposable.Dispose()
+            {
+                LogFactory.Dispose();
+            }
+        }
+        private readonly Fixture _fixture;
+      public ESIRequestTests(Fixture fixture)
       {
         _fixture = fixture;
       }
